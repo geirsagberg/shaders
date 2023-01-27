1bd4453d42513ea37c987ed00539d442a1df3edf
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2023-01-27 13:43:10 +0100

    Add simple three.js example

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,39 @@
 import "./style.css";
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+} from "three";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    Hello World!
-  </div>
-`;
+const app = document.querySelector("#app")!;
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth / 1.5, window.innerHeight / 1.5);
+app.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+};
+
+animate();
