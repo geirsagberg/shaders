3f0b22838687e2c659a9874075f20186c0d8b567
Geir Sagberg <geir.sagberg@gmail.com>
2023-02-22 15:04:01 +0100

    Render 3d

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -2,11 +2,7 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main() {
-  vec2 center = u_resolution / 2.0;
+  float color = 1.0;
 
-  float distanceFromCenter = mod(distance(gl_FragCoord.xy, center), u_time);
-
-  float color = distanceFromCenter > 1. ? 1.0 : 0.0;
-
-  gl_FragColor = vec4(1. - color, color - 1., mod(pow(color, 2.), 1.0), 1.0);
+  gl_FragColor = vec4(vec3(color), 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -2,5 +2,7 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main() {
-  gl_Position = vec4(position, 0.51);
+
+  // Transform vertex position
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
